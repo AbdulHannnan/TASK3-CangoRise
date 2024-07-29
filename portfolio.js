aca7ad0f175c1c06680f0a9c358c7adaee996279
Abdul Hannan <ahk12067@gmail.com>
2024-07-29 20:15:39 +0500

    portfolio

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+// const menu = document.querySelector(#menu);
+// const nav =  document.querySelector(.links);
+
+// menu.onclick = () =>{
+//     menu.classlist.toggle("bx-x");
+//     nav.classlist.toggle('active');
+// }
